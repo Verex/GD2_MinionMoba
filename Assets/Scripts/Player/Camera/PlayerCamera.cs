7ce13488a70263a51c386bd716c7a31d42427be4
Zachary Luebke <zach.verex@gmail.com>
2018-09-20 14:14:30 -0400

    Updated stuff.

---
+++ Assets/Scripts/Player/Camera/PlayerCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private float moveTime = 1.0f;
+    [SerializeField] private float moveSpeed = 1.0f;
+    [SerializeField] private float maxSpeed = 1.0f;
+    [SerializeField] private Vector3 moveDirection = Vector3.zero;
+
+    private Vector3 currentVelocity = Vector3.zero;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        // Check if we have assigned move direction.
+        if (moveDirection != Vector3.zero)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position,
+                transform.position + (moveDirection * moveSpeed), ref currentVelocity, 0.2f, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Input/PlayerActions.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using InControl;
+
+public class PlayerActions : PlayerActionSet {
+    public PlayerAction Left;
+    public PlayerAction Right;
+    public PlayerAction Up;
+    public PlayerAction Down;
+    public PlayerTwoAxisAction Move;
+
+    public PlayerActions() {
+        Left = CreatePlayerAction("Move Left");
+        Right = CreatePlayerAction("Move Right");
+        Up = CreatePlayerAction("Move Forward");
+        Down = CreatePlayerAction("Move Backward");
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+    }
+
+    public static PlayerActions CreateWithDefaultBindings() {
+        var playerActions = new PlayerActions();
+
+        playerActions.Up.AddDefaultBinding(Key.W);
+        playerActions.Down.AddDefaultBinding(Key.S);
+        playerActions.Left.AddDefaultBinding(Key.A);
+        playerActions.Right.AddDefaultBinding(Key.D);
+
+        playerActions.ListenOptions.IncludeUnknownControllers = true;
+        playerActions.ListenOptions.MaxAllowedBindings = 4;
+        //playerActions.ListenOptions.MaxAllowedBindingsPerType = 1;
+        //playerActions.ListenOptions.AllowDuplicateBindingsPerSet = true;
+        playerActions.ListenOptions.UnsetDuplicateBindingsOnSet = true;
+        //playerActions.ListenOptions.IncludeMouseButtons = true;
+        //playerActions.ListenOptions.IncludeModifiersAsFirstClassKeys = true;
+        //playerActions.ListenOptions.IncludeMouseButtons = true;
+        //playerActions.ListenOptions.IncludeMouseScrollWheel = true;
+
+        playerActions.ListenOptions.OnBindingFound = (action, binding) => {
+            if (binding == new KeyBindingSource(Key.Escape)) {
+                action.StopListeningForBinding();
+                return false;
+            }
+            return true;
+        };
+
+        playerActions.ListenOptions.OnBindingAdded += (action, binding) => {
+            Debug.Log("Binding added... " + binding.DeviceName + ": " + binding.Name);
+        };
+
+        playerActions.ListenOptions.OnBindingRejected += (action, binding, reason) => {
+            Debug.Log("Binding rejected... " + reason);
+        };
+
+        return playerActions;
+    }
+}
+
